00b285750cabf18ae1f1a4838e9e3a7f268e70dc
Arn <arnarnsde@gmail.com>
2025-07-12 09:00:05 +0600

    layout added to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 'use client';
 
+import Layout from '@/components/common/Layout/Layout';
 import HeroSection from "@/components/Home/HeroSection/HeroSection";
 import WhyUs from '@/components/Home/WhyUs/WhyUs';
 import StatsBar from '@/components/Home/StatsBar/StatsBar';
@@ -12,13 +13,15 @@ import SocialRow from '@/components/Home/SocialRow/SocialRow';
 export default function HomePage() {
   return (
     <main className="min-h-screen">
-      <HeroSection />
-      <WhyUs />
-      <StatsBar />
-      <ProductDemoCarousel />
-      <NewsroomTicker />
-      <SupportOverview />
-      <SocialRow />
+      <Layout>
+        <HeroSection />
+        <WhyUs />
+        <StatsBar />
+        <ProductDemoCarousel />
+        <NewsroomTicker />
+        <SupportOverview />
+        <SocialRow />
+      </Layout>
     </main>
   );
 }
